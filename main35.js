a1c0dc78da66a4eb2cb8e878172ea0b25ad54c01
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 09:31:25 -0700

    35-Assignment
    TypeScript 45-Assignment of Q35 with Node.js

---
+++ main35.js
@@ -0,0 +1,18 @@
+"use strict";
+// ASSIGNMENT NO 35:
+/*
+Animals: Think of at least three different animals that have a common characteristic.
+ Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+  • Modify your program to print a statement about each animal, such as A dog would make a great pet.
+  • Add a line at the end of your program stating what these animals have in common.
+ You could print a sentence such as Any of these animals would make a great pet!
+*/
+console.log("ASSIGNMENT NO 35:");
+// Creating a Array:
+let petAnimals = ["Cat", "Dog", "Rabbit"];
+// Using For-Loop:
+for (let onePet of petAnimals) {
+    console.log(`A ${onePet} would make a great pet.`);
+}
+// Print a Message outside of For-Loop:
+console.log("Any of these animals would  make a great pet!\n");
